0d983f63e0dd0c4483f5d7080c5b621d652194df
G-B-FutureMarti <81188293+G-B-FutureMartian@users.noreply.github.com>
2022-11-03 21:16:21 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,16 @@
+// Variable definitions
+let lvl1 = ","
+let lvl2 = " "
+let lvl3 = "3"
+let lvl4 = "7"
+var score: Int
+
+// Functions
+func Append(_ string: String, file: String) {
+  File = OutputStream(url:file,append:true)
+  File.write(string)
+}
+// Playing the game
+while score>(-5) {
+  
+}
