c5e3181ae46201b160c06f94678d76fc61625e28
G-B-FutureMarti <81188293+G-B-FutureMartian@users.noreply.github.com>
2022-11-03 22:25:02 +0000

    Squashed bugs, squashed bugs, and made it so you can play all the levels.

--- main.swift
+++ main.swift
@@ -5,6 +5,8 @@ let lvl3 = "3"
 let lvl4 = "7"
 var score: Int
 var input = readLine()
+var currentLevel = 1
+
 // Functions
 /*func Append(_ string: String, file: String) {
   do {
@@ -23,4 +25,52 @@ score = 0 // I initialized the variable seperatly because I would like to add a 
           // It's easier this way
 while score>(-5) { 
   input = readLine()
+  switch(input) {
+    case ",":
+      if currentLevel == 1 {
+        score += 1
+        currentLevel += 1
+        
+        print("Score = ",score)
+        if score<0 {
+          print("You lose all of your progress if you get a score of -5")
+        }
+        print("Level: ", currentLevel)
+      }
+    case " ":
+      if currentLevel == 2 {
+        score += 1
+        currentLevel += 1
+        
+        print("Score = ",score)
+        if score<0 {
+          print("You lose all of your progress if you get a score of -5")
+        }
+        print("Level: ", currentLevel)
+      }
+    case "3":
+      if currentLevel == 3 {
+        score += 1
+        currentLevel += 1
+        
+        print("Score = ",score)
+        if score<0 {
+          print("You lose all of your progress if you get a score of -5")
+        }
+        print("Level: ", currentLevel)
+      }
+    case "7":
+      if currentLevel == 4 {
+          score += 1
+          currentLevel += 1
+          
+          print("Score = ",score)
+          if score<0 {
+            print("You lose all of your progress if you get a score of -5")
+          }
+          print("Level: ", currentLevel)
+        }
+    default:
+      print("Incorrect")
+  } 
 }
