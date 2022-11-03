f187b90c983bf9affbc9cd013dc323e21063fffd
G-B-FutureMarti <81188293+G-B-FutureMartian@users.noreply.github.com>
2022-11-03 21:48:59 +0000

    Commented out unimportant stuff, deleted some code squashed several bugs.

--- main.swift
+++ main.swift
@@ -4,13 +4,23 @@ let lvl2 = " "
 let lvl3 = "3"
 let lvl4 = "7"
 var score: Int
-
+var input = readLine()
 // Functions
-func Append(_ string: String, file: String) {
+/*func Append(_ string: String, file: String) {
+  do {
+    try {
+      if File != nil {}
+    }
+  } catch {
+    var File: String
+  }
   File = OutputStream(url:file,append:true)
   File.write(string)
-}
+}*/
+
 // Playing the game
-while score>(-5) {
-  
+score = 0 // I initialized the variable seperatly because I would like to add a way to save progress
+          // It's easier this way
+while score>(-5) { 
+  input = readLine()
 }
